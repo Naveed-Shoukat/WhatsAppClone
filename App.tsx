efaea128d29da4f9fb1a5fe973e7542ffec7ea6a
naveed-shoukat <shoukat.naveed@gmail.com>
2022-07-25 17:10:45 +0300

    authentication using withAuthenticator  aws-amplify-react-native

--- App.tsx
+++ App.tsx
@@ -5,11 +5,14 @@ import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
+//@ts-ignore
+import { withAuthenticator } from 'aws-amplify-react-native';
+
 import { Amplify } from 'aws-amplify';
 import awsconfig from './src/aws-exports';
 Amplify.configure(awsconfig);
 
-export default function App() {
+function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
@@ -24,3 +27,4 @@ export default function App() {
     );
   }
 }
+export default withAuthenticator(App);
